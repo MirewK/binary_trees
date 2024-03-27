087ab11e2db198c3b21dbeb78ded5ce5b1e333df
MirewK <meremehertab@gmail.com>
2024-03-27 15:02:32 -0700

    binary tree balance function correction

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
         {
                 size_t x = 0, y = 0;
 
-                x = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-                y = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+                x = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+                y = tree->right ? 1 + binary_tree_height(tree->right) : 1;
                 return ((x > y) ? x : y);
         }
         return (0);
